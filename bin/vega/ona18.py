6f34afce4d508c8626ec3efacb255b50d85a7b1c
Su Hang <constellationcolon@gmail.com>
2020-10-18 19:22:50 -0400

    add ona18 data

---
+++ bin/vega/ona18.py
@@ -0,0 +1,171 @@
+import random
+from itertools import chain
+from operator import itemgetter
+from typing import List
+
+import altair as alt
+import pandas as pd
+from vega_datasets import data
+
+
+COLORS = dict(
+    LIGHT=[
+        "#AC0A00",
+        "#FFBD10",
+        "#370855",
+        "#4F8F00",
+        "#DD5300",
+        "#81007F",
+        "#651A01",
+        "#929000",
+        "#240D00",
+        "#9F760B",
+    ],
+    DARK=[
+        "#FF2F92",
+        "#946B2D",
+        "#2647B0",
+        "#247F32",
+        "#4F6DD3",
+        "#5D9B66",
+        "#D0D0D0",
+        "#89CCEB",
+        "#BAB27D",
+        "#FF7E79",
+    ],
+)
+
+
+ESSAYER_BASE_CONFIG = {
+    "width": "container",
+    "height": "container",
+    "config": {
+        "background": None,
+        "title": {
+            "font": "Space Mono",
+            "subtitleFont": "Space Mono",
+            "fontWeight": "normal",
+        },
+        "axis": {
+            "labelFont": "Space Mono",
+            "titleFont": "Space Mono",
+            "titleFontWeight": "normal",
+            "titlePadding": 10,
+        },
+        "legend": {
+            "labelFont": "Space Mono",
+            "titleFont": "Space Mono",
+            "titleFontWeight": "normal",
+        },
+        "text": {"font": "Space Mono"},
+    },
+}
+
+
+def essayer_light():
+    config = ESSAYER_BASE_CONFIG.copy()
+    config["config"].update({"range": {"category": COLORS["LIGHT"]}})
+    return config
+
+
+def essayer_dark():
+    config = ESSAYER_BASE_CONFIG.copy()
+    config["config"]["group"] = {"strokeOpacity": 0}
+    config["config"]["range"] = {"category": COLORS["DARK"]}
+    config["config"]["title"]["color"] = "white"
+    config["config"]["style"] = {
+        "guide-label": {"fill": "white"},
+        "guide-title": {"fill": "white"},
+    }
+    config["config"]["axis"].update(
+        {"domainColor": "white", "gridColor": "#212121", "tickColor": "white"}
+    )
+    return config
+
+
+alt.themes.register("essayer-light", essayer_light)
+alt.themes.register("essayer-dark", essayer_dark)
+THEMES = ["light", "dark"]
+
+df = (
+    pd.read_csv("assets/csv/ona18.csv", index_col=None)
+    .rename(
+        columns={
+            "direct": "Direct",
+            "fb": "Facebook",
+            "goog": "Google",
+            "gcs": "Google Chrome Suggestions",
+            "goognews": "Google News",
+            "twtr": "Twitter",
+            "flipboard": "Flipboard",
+            "outbrain": "Outbrain",
+            "insta": "Instagram",
+            "yahoo": "Yahoo",
+            "date": "DATE",
+        }
+    )
+    .melt("DATE", var_name="REFERRER", value_name="REFERRALS")
+    .assign(
+        **{"WEEKLY REFERRALS (BILLIONS)": lambda df: df["REFERRALS"] / 1e9}
+    )
+)
+
+sort_order = (
+    df.groupby("REFERRER")["REFERRALS"]
+    .sum()
+    .sort_values(ascending=False)
+    .index.tolist()
+)
+
+
+def plot(theme: str):
+    highlight = alt.selection(
+        type="single", on="mouseover", fields=["REFERRER"], nearest=True
+    )
+    nearest = alt.selection(
+        type="single",
+        nearest=True,
+        on="mouseover",
+        fields=["REFERRER", "DATE"],
+        empty="none",
+    )
+
+    base = (
+        alt.Chart(df)
+        .encode(
+            x=alt.X("DATE:T", axis=alt.Axis(format="%b %y")),
+            y="WEEKLY REFERRALS (BILLIONS):Q",
+        )
+        .properties(title="REFERRED MOBILE VISITS TO NEWS SITES, BY SOURCE")
+    )
+    points = (
+        base.mark_circle()
+        .encode(opacity=alt.value(0))
+        .add_selection(highlight)
+        .add_selection(nearest)
+    )
+    lines = base.mark_line().encode(
+        size=alt.condition(~highlight, alt.value(1), alt.value(3)),
+        color=alt.Color(
+            "REFERRER:N",
+            legend=alt.Legend(
+                orient="bottom", columns=4, direction="horizontal"
+            ),
+            sort=sort_order,
+        ),
+    )
+    text = lines.mark_text(
+        align="left", baseline="line-bottom", dx=5, dy=-5
+    ).encode(
+        text=alt.condition(nearest, "REFERRER:N", alt.value(" ")),
+        size=alt.value(10),
+    )
+
+    chart = points + text + lines
+
+    chart.save(f"assets/vega/ona18-{theme}.json")
+
+
+for theme in THEMES:
+    with alt.themes.enable(f"essayer-{theme}"):
+        plot(theme)
