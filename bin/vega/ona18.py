596b76c814a960b9f42e7c2123530123debb0398
Su Hang <constellationcolon@gmail.com>
2020-10-29 23:40:38 -0400

    style upgrades

--- bin/vega/ona18.py
+++ bin/vega/ona18.py
@@ -7,6 +7,15 @@ import altair as alt
 import pandas as pd
 from vega_datasets import data
 
+ICE = "rgb(241,247,249)"
+FOG = "rgb(219,220,228)"
+STEEL = "rgb(161,163,173)"
+STORMBORN = "rgb(76,78,101)"
+
+CHOCOLATE = "rgb(11,5,1)"
+CEDAR = "rgb(32,25,11)"
+OLIVE = "rgb(77,66,44)"
+PARCHMENT = "rgb(208,168,122)"
 
 ESSAYER_BASE_CONFIG = {
     "width": "container",
@@ -14,6 +23,7 @@ ESSAYER_BASE_CONFIG = {
     "config": {
         "background": None,
         "font": "Space Mono",
+        "group": {"strokeOpacity": 0},
         "title": {
             "font": "Space Mono",
             "subtitleFont": "Space Mono",
@@ -38,23 +48,38 @@ ESSAYER_BASE_CONFIG = {
 
 def essayer_light():
     config = ESSAYER_BASE_CONFIG.copy()
-    config["config"]["mark"] = {"color": "black"}
-    config["config"]["text"] = {"color": "black"}
+    config["config"]["mark"] = {"color": STORMBORN}
+    config["config"]["text"] = {"color": STORMBORN}
+    config["config"]["title"]["color"] = STORMBORN
+    config["config"]["style"] = {
+        "guide-label": {"fill": STORMBORN},
+        "guide-title": {"fill": STORMBORN},
+    }
+    config["config"]["axis"].update(
+        {
+            "domainColor": STORMBORN,
+            "gridColor": FOG,
+            "tickColor": STORMBORN,
+        }
+    )
     return config
 
 
 def essayer_dark():
     config = ESSAYER_BASE_CONFIG.copy()
-    config["config"]["group"] = {"strokeOpacity": 0}
-    config["config"]["mark"] = {"color": "white"}
-    config["config"]["text"] = {"color": "white"}
-    config["config"]["title"]["color"] = "white"
+    config["config"]["mark"] = {"color": PARCHMENT}
+    config["config"]["text"] = {"color": PARCHMENT}
+    config["config"]["title"]["color"] = PARCHMENT
     config["config"]["style"] = {
-        "guide-label": {"fill": "white"},
-        "guide-title": {"fill": "white"},
+        "guide-label": {"fill": PARCHMENT},
+        "guide-title": {"fill": PARCHMENT},
     }
     config["config"]["axis"].update(
-        {"domainColor": "white", "gridColor": "#212121", "tickColor": "white"}
+        {
+            "domainColor": PARCHMENT,
+            "gridColor": CEDAR,
+            "tickColor": PARCHMENT,
+        }
     )
     return config
 
